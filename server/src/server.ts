2cfdc39852535f6b643792600e466fd4e349d1b4
Monique Romão <romaomonique4@gmail.com>
2023-06-07 17:12:30 -0300

    build: project structure

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    response.json([
+        {id: 1, nome: 'anúncio 1'},
+        {id: 2, nome: 'anúncio 2'},
+        {id: 3, nome: 'anúncio 3'},
+        {id: 4, nome: 'anúncio 4'},
+    ])
+})
+
+app.listen(3333)
